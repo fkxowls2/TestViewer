e673eb0c073fb24d7061b675399b27ff63996c1c
racoding <fkxowls@gmail.com>
2023-02-16 10:58:42 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace TestViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox0 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox0, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox5, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox6, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox7, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1178, 582);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Location = new System.Drawing.Point(924, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(251, 285);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(667, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(251, 285);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(410, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(251, 285);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.button2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.listBox1, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.button3, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(144, 285);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(3, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "이미지 보여주기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "이미지 불러오기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(3, 123);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(138, 271);
+            this.listBox1.TabIndex = 2;
+            // 
+            // pictureBox0
+            // 
+            this.pictureBox0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox0.Location = new System.Drawing.Point(153, 3);
+            this.pictureBox0.Name = "pictureBox0";
+            this.pictureBox0.Size = new System.Drawing.Size(251, 285);
+            this.pictureBox0.TabIndex = 1;
+            this.pictureBox0.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox4.Location = new System.Drawing.Point(153, 294);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(251, 285);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox5.Location = new System.Drawing.Point(410, 294);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(251, 285);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox6.Location = new System.Drawing.Point(667, 294);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(251, 285);
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox7.Location = new System.Drawing.Point(924, 294);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(251, 285);
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.listBox2, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 294);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(144, 285);
+            this.tableLayoutPanel3.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "상태창";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(3, 43);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(138, 312);
+            this.listBox2.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(3, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Draw 시간 계산";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 582);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Test Viewer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox0;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TestViewer
+{
+    public partial class Form1 : Form
+    {
+        private delegate void MyFunction();
+        private List<Image> imageList = new List<Image>();
+        private int listNumber;
+        private DateTime loadStartTime;
+        private DateTime loadEndTime;
+        private DateTime drawStartTime;
+        private DateTime drawEndTime;
+        private int timeCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+
+            // OpenFileDialog 생성
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // 이미지 파일만 필터링
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+
+            // 멀티 선택 가능
+            openFileDialog1.Multiselect = true;
+
+            // 대화 상자가 열리면
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // ListBox에 선택한 파일 경로 추가
+                foreach (string fileName in openFileDialog1.FileNames)
+                {
+                    listBox1.Items.Add(fileName);
+                }
+            }
+
+            listNumber = listBox1.Items.Count;
+            if (listNumber > 8)
+            {
+                listNumber = 8;
+            }
+
+            Thread t = new Thread(new ThreadStart(ImageLoad));
+            t.IsBackground = true;
+            listBox2.Items.Add("이미지 로드 시작");
+            t.Start();
+
+        }
+
+        private void ImageLoad()
+        {
+            loadStartTime = DateTime.Now;
+            
+            for (int i = 0; i < listNumber; i++)
+            {
+                imageList.Add(Image.FromFile(listBox1.Items[i].ToString()));
+            }
+
+            loadEndTime = DateTime.Now;
+            TimeSpan duration = loadEndTime - loadStartTime;
+
+            Invoke(new Action(() =>
+            {
+                listBox2.Items.Add("이미지 로드 완료");
+                listBox2.Items.Add($"이미지 로드 시간: {duration.TotalMilliseconds} ms");
+                button2.Enabled = true;
+            }));
+        }
+
+        private void ClearAll()
+        {
+            listBox1.Items.Clear();
+            imageList.Clear();
+            ClearPicture();
+            GC.Collect(0);
+        }
+
+        private void ClearPicture()
+        {
+            pictureBox0.Image = null;
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            pictureBox3.Image = null;
+            pictureBox4.Image = null;
+            pictureBox5.Image = null;
+            pictureBox6.Image = null;
+            pictureBox7.Image = null;
+            pictureBox0.Refresh();
+            pictureBox1.Refresh();
+            pictureBox2.Refresh();    
+            pictureBox3.Refresh();
+            pictureBox4.Refresh();
+            pictureBox5.Refresh();
+            pictureBox6.Refresh();
+            pictureBox7.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+
+            List<MyFunction> functionList = new List<MyFunction>();
+            functionList.Add(Function0);
+            functionList.Add(Function1);
+            functionList.Add(Function2);
+            functionList.Add(Function3);
+            functionList.Add(Function4);
+            functionList.Add(Function5);
+            functionList.Add(Function6);
+            functionList.Add(Function7);
+
+            ClearPicture();
+
+            for (int i = 0; i < listNumber; i++)
+            {
+                functionList[i]();
+            }
+            button3.Enabled = true;
+        }
+
+        private void Function0()
+        {
+            Thread t0 = new Thread(new ThreadStart(Draw0));
+            t0.IsBackground = true;
+            t0.Start();
+        }
+
+        private void Function1()
+        {
+            Thread t1 = new Thread(new ThreadStart(Draw1));
+            t1.IsBackground = true;
+            t1.Start();
+        }
+
+        private void Function2()
+        {
+            Thread t2 = new Thread(new ThreadStart(Draw2));
+            t2.IsBackground = true;
+            t2.Start();
+        }
+
+        private void Function3()
+        {
+            Thread t3 = new Thread(new ThreadStart(Draw3));
+            t3.IsBackground = true;
+            t3.Start();
+        }
+
+        private void Function4()
+        {
+            Thread t4 = new Thread(new ThreadStart(Draw4));
+            t4.IsBackground = true;
+            t4.Start();
+        }
+
+        private void Function5()
+        {
+            Thread t5 = new Thread(new ThreadStart(Draw5));
+            t5.IsBackground = true;
+            t5.Start();
+        }
+
+        private void Function6()
+        {
+            Thread t6 = new Thread(new ThreadStart(Draw6));
+            t6.IsBackground = true;
+            t6.Start();
+        }
+
+        private void Function7()
+        {
+            Thread t7 = new Thread(new ThreadStart(Draw7));
+            t7.IsBackground = true;
+            t7.Start();
+        }
+
+        private void Draw0()
+        {
+            DateTime startTime = DateTime.Now;
+
+            Invoke(new Action(() =>
+            {
+                pictureBox0.Image = imageList[0];
+                pictureBox0.SizeMode = PictureBoxSizeMode.Zoom;
+            }));
+
+            DateTime endTime = DateTime.Now;
+            DrawTimeCounter(startTime, endTime);
+        }
+
+        private void Draw1()
+        {
+            DateTime startTime = DateTime.Now;
+
+            Invoke(new Action(() =>
+            {
+                pictureBox1.Image = imageList[1];
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }));
+
+            DateTime endTime = DateTime.Now;
+            DrawTimeCounter(startTime, endTime);
+        }
+
+        private void Draw2()
+        {
+            DateTime startTime = DateTime.Now;
+
+            Invoke(new Action(() =>
+            {
+                pictureBox2.Image = imageList[2];
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }));
+
+            DateTime endTime = DateTime.Now;
+            DrawTimeCounter(startTime, endTime);
+        }
+
+        private void Draw3()
+        {
+            DateTime startTime = DateTime.Now;
+
+            Invoke(new Action(() =>
+            {
+                pictureBox3.Image = imageList[3];
+                pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            }));
+
+            DateTime endTime = DateTime.Now;
+            DrawTimeCounter(startTime, endTime);
+        }
+
+        private void Draw4()
+        {
+            DateTime startTime = DateTime.Now;
+
+            Invoke(new Action(() =>
+            {
+                pictureBox4.Image = imageList[4];
+                pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            }));
+
+            DateTime endTime = DateTime.Now;
+            DrawTimeCounter(startTime, endTime);
+        }
+
+        private void Draw5()
+        {
+            DateTime startTime = DateTime.Now;
+
+            Invoke(new Action(() =>
+            {
+                pictureBox5.Image = imageList[5];
+                pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            }));
+
+            DateTime endTime = DateTime.Now;
+            DrawTimeCounter(startTime, endTime);
+        }
+
+        private void Draw6()
+        {
+            DateTime startTime = DateTime.Now;
+
+            Invoke(new Action(() =>
+            {
+                pictureBox6.Image = imageList[6];
+                pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            }));
+
+            DateTime endTime = DateTime.Now;
+            DrawTimeCounter(startTime, endTime);
+        }
+
+        private void Draw7()
+        {
+            DateTime startTime  = DateTime.Now;
+            
+            Invoke(new Action(() =>
+            {
+                pictureBox7.Image = imageList[7];
+                pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            }));
+
+            DateTime endTime = DateTime.Now;
+            DrawTimeCounter(startTime, endTime);
+        }
+
+        private void DrawTimeCounter(DateTime startTime, DateTime endTime)
+        {
+            if(timeCount == 0)
+            {
+                drawStartTime = startTime;
+            }
+            drawEndTime= endTime;
+            timeCount++;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (timeCount == listNumber)
+            {
+                button3.Enabled = false;
+                TimeSpan duration = drawEndTime - drawStartTime;
+                listBox2.Items.Add($"Draw 총 시간: {duration.TotalMilliseconds} ms");
+                timeCount = 0;
+                Thread.Sleep(2000);
+                button2.Enabled = true;
+            }
+            else
+            {
+                listBox2.Items.Add("아직 Drwaing이 끝나지 않았습니다.");
+            }
+        }
+    }
+}
